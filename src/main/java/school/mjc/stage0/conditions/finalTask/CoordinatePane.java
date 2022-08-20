24521d40daa53d2b79913fee3c2a5ee87c4cb86d
dveltauri <diana.veltauri1@gmail.com>
2022-08-20 13:33:13 -0700

    Init commit

--- src/main/java/school/mjc/stage0/conditions/finalTask/CoordinatePane.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/CoordinatePane.java
@@ -2,5 +2,18 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class CoordinatePane {
     public void printQuadrant(int x, int y) {
+        String pos ;
+        if(x == 0 & y == 0){
+            pos = "zero";
+        } else if (x >0 && y > 0) {
+            pos = "first";
+        } else if (x < 0 && y > 0) {
+            pos = "second";
+        }else if (x <0 && y < 0){
+            pos = "third";
+        } else {
+            pos ="fourth";
+        }
+        System.out.println(pos);
     }
 }
--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,6 +2,39 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
+        if(year <= 0 || month <= 0 || month > 12){
+            System.out.println("invalid date");
+        }else {
+            boolean isLeap = isLeapYear(year);
+            System.out.println(daysInMonth(isLeap,month));
+        }
+    }
 
+    private int daysInMonth(boolean isLeap, int month){
+        switch (month){
+            case 1, 3, 5, 7, 8, 10, 12 :
+                return 31;
+            case 4, 6, 9, 11:
+                return 30;
+            default:
+                return isLeap ? 29 : 28;
+        }
+    }
+
+    private boolean isLeapYear(int year){
+        switch (year%4){
+            case 0:
+                switch (year%100){
+                    case 0:
+                        switch (year%400){
+                            case 0:
+                                return true;
+                            default:
+                                return false;
+                        }
+                    default: return true;
+                }
+            default:return false;
+        }
     }
 }
